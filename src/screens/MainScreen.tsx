fdf85a65813f189a3c9e78d0892fd892fb6a4b71
SophieRF <sofiaferraro2110@gmail.com>
2025-03-26 10:55:37 -0300

    unCommit

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,17 +1,23 @@
 import { Col, Container, Row } from "react-bootstrap"
-import { NavBar } from "../components/NavBar/NavBar"
-import { Home } from "../components/Home/Home"
-import { SideBar } from "../components/SideBar/SideBar"
+import { NavBar } from "../ui/NavBar/NavBar"
+import { Home } from "../ui/Home/Home"
+import { SideBar } from "../ui/SideBar/SideBar"
 
 export const MainScreen = () => {
     return (
         <Container>
             <Row>
-                <Col><NavBar /></Col>
+                <Col>
+                    <NavBar />
+                </Col>
             </Row>
             <Row>
-                <Col md={2}><SideBar /></Col>
-                <Col md={10}><Home /></Col>
+                <Col md={2}>
+                    <SideBar />
+                </Col>
+                <Col md={10}>
+                    <Home />
+                </Col>
             </Row>
         </Container>
     )
